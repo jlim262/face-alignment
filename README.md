ed1552001dcad39eeae6b10224a125ed62d500be
jlim <seedengine@gmail.com>
2020-03-26 15:44:36 +1300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # face-alignment
+
+## Introduction
+This project is an utility for alignment of a human face from an image. 
+
+## Prerequisites
+- Python 3.7
+
+## Installation
+```bash
+git clone https://github.com/jlim262/face-alignment.git
+```
+```bash
+cd face-alignment
+```
+```bash
+pip install -r requirements.txt
+```
+
+## How to run
+
+## Directory hierachy
+
+## Known Issues
+If you face the following error when you run face_aligner.py
+```bash
+qt.qpa.plugin: Could not find the Qt platform plugin "cocoa" in ""
+This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
+```
+please try to run this command in your terminal. 
+```bash
+pip install opencv-python-headless
+```
+
+## References
+- [ExtractFaceChip.py](https://gist.github.com/zalo/fa4396ae7a72b7683888fd9cd1c6d920)
+- [https://www.pyimagesearch.com/2017/05/22/face-alignment-with-opencv-and-python/](https://www.pyimagesearch.com/2017/05/22/face-alignment-with-opencv-and-python/)
---
+++ face_aligner.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+from enum import Enum
+import numpy as np
+import cv2
+import face_alignment
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from skimage import io
+
+
+class LandmarksType(Enum):
+    """Enum class defining the type of landmarks to detect.
+
+    ``_2D`` - the detected points ``(x,y)`` are detected in a 2D space and follow the visible contour of the face
+    ``_2halfD`` - this points represent the projection of the 3D points into 3D
+    ``_3D`` - detect the points ``(x,y,z)``` in a 3D space
+
+    """
+    _2D = 1
+    _2halfD = 2
+    _3D = 3
+
+
+class FaceAligner:
+    def __init__(self, output_width, output_height=None, landmarks_type=LandmarksType._3D, device='cuda', flip_input='False', face_detector='sfd'):
+        self.output_width = output_width
+        self.output_height = self.output_width if output_height is None else output_height
+        self.output_rect = np.float32([
+                            [0, 0],
+                            [self.output_height, 0],
+                            [0, self.output_width],
+                            [self.output_height, self.output_width]])
+        self.landmarks_type = self._to_fa_type(landmarks_type)
+        self.fa = face_alignment.FaceAlignment(self.landmarks_type, flip_input=flip_input, device=device, face_detector=face_detector)
+
+    def align_from_image(self, image):
+        predictions = self.fa.get_landmarks_from_image(image)
+        if(predictions is None):
+            return None
+
+        outputs = []
+        for prediction in predictions:
+            # Compute the Anchor Landmarks
+            # This ensures the eyes and chin will not move within the output
+            right_eye_mean = np.mean(prediction[36:42], axis=0)
+            left_eye_mean = np.mean(prediction[42:47], axis=0)
+            middle_of_eye = (right_eye_mean + left_eye_mean) * 0.5
+            chin = prediction[8]
+
+            # Compute the output center and up/side vectors
+            mean = ((middle_of_eye * 3) + chin) * 0.25
+            centered = prediction - mean 
+            right_vector = (left_eye_mean - right_eye_mean)
+            up_vector    = (chin        - middle_of_eye)
+
+            # Divide by the length ratio to ensure a square aspect ratio
+            right_vector /= np.linalg.norm(right_vector) / np.linalg.norm(up_vector)
+
+            # Compute the corners of the facial output
+            image_corners = np.float32([(mean + ((-right_vector - up_vector)))[:2],
+                                       (mean + (( right_vector - up_vector)))[:2],
+                                       (mean + ((-right_vector + up_vector)))[:2],
+                                       (mean + (( right_vector + up_vector)))[:2]])
+
+            # Compute the Perspective Homography and Extract the output from the image
+            transform = cv2.getPerspectiveTransform(image_corners, self.output_rect)
+            output = cv2.warpPerspective(image, transform, (self.output_height, self.output_width))
+            outputs.append(output)
+        
+        return outputs
+        
+
+    def align_from_directory(self, path, extensions=['.jpg', '.png'], recursive=True, show_progress_bar=True):
+        pass
+
+    def _to_fa_type(self, type):
+        if(type == LandmarksType._2D):
+            return face_alignment.LandmarksType._2D
+        elif(type == LandmarksType._2halfD):
+            return face_alignment.LandmarksType._2halfD
+        elif(type == LandmarksType._3D):
+            return face_alignment.LandmarksType._3D
+
+if __name__ == '__main__':
+    fa = FaceAligner(256, device='cpu')
+    input_img = io.imread('./test/aflw-test.jpg')
+    outputs = fa.align_from_image(input_img)
+    plt.imshow(outputs[0])
+    plt.show()
